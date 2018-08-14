75af489c6ca237a366442ef0a0bc51299a8aa7c3
copas <copas2@gmail.com>
2018-08-14 20:10:26 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import cellEditFactory from 'react-bootstrap-table2-editor';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div style={{width: 400}}>
+        <BootstrapTable striped hover
+          keyField="id"
+          columns={ [
+            { dataField: 'id', text: 'ID'}, 
+            { dataField: 'name', text: 'Name'}
+          ] }
+          data={ [
+            {id:1, name: "one"},
+            {id:2, name: "two"},
+            {id:3, name: "three"},
+            {id:4, name: "four"},
+            {id:5, name: "five"},
+            {id:6, name: "six"},
+          ] }
+          cellEdit={ cellEditFactory({ mode: 'click' }) }
+          pagination={ paginationFactory({
+            sizePerPage: 5,
+            totalSize: 6,
+          }) }  
+        />
+      </div>
+    );
+  }
+}
